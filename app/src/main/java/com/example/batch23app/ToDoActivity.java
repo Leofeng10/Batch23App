a5dcb99b2038decc248cb508c7542070ae60b2e2
Leofeng10 <68378928+Leofeng10@users.noreply.github.com>
2021-06-04 22:54:30 +0800

    Google Login

---
+++ app/src/main/java/com/example/batch23app/ToDoActivity.java
@@ -0,0 +1,14 @@
+package com.example.batch23app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ToDoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do);
+    }
+}
