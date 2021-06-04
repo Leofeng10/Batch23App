6499a406641f13acd0d3edb3938895b1fc1d9e70
EvaderFati <evaderfati@gmail.com>
2021-06-05 07:59:56 +0800

    add facebook login
     Committer: Evader Fati <evaderfati@gmail.com>
    
     On branch andy
     Changes to be committed:
    	modified:   .idea/compiler.xml
    	modified:   .idea/gradle.xml
    	modified:   .idea/misc.xml
    	new file:   .idea/runConfigurations.xml
    	modified:   app/build.gradle
    	modified:   app/src/main/AndroidManifest.xml
    	modified:   app/src/main/java/com/example/batch23app/MainActivity.java
    	modified:   app/src/main/res/layout/activity_main.xml
    	modified:   app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/batch23app/MainActivity.java
+++ app/src/main/java/com/example/batch23app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.batch23app;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,7 +8,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -15,11 +23,17 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     GoogleSignInClient mGoogleSignInClient;
 
     private final int RC_SIGN_IN = 1;
+    private ImageView facebookSignin;  // facebook signin
+    private LoginButton loginManager;  // facebook signin
+
+    CallbackManager callbackManager;    // facebook signin
 
     ImageView googleSignin;
     @Override
@@ -34,6 +48,9 @@ public class MainActivity extends AppCompatActivity {
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         googleSignin = findViewById(R.id.google_signin);
+        facebookSignin = findViewById(R.id.facebook_signin);
+        loginManager = findViewById(R.id.login_button);
+        callbackManager = CallbackManager.Factory.create(); // facebook signin
 
         googleSignin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,8 +59,33 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(signInIntent, RC_SIGN_IN);
             }
         });
+
+        loginManager.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // handle success
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
     }
 
+    public void onClick(View v) {
+        if (v == facebookSignin) {
+            LoginManager.getInstance().logInWithReadPermissions(
+                    this,
+                    Arrays.asList("user_photos", "email", "user_birthday", "public_profile")
+            );
+        }
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -56,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
         }
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
